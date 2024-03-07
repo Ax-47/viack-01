6b807fa911de270e9506aadca02416076bd9ea4d
Mikhail Akhsakov <85576471+Ax-47@users.noreply.github.com>
2024-03-07 17:48:15 +0700

    Update sketch_feb25a.ino

--- sketch_feb25a.ino
+++ sketch_feb25a.ino
@@ -27,25 +27,19 @@
 #include "JoSlayer.h"
 
 //pin setup
-#define measurePin 36 //analog pin
-#define ledPower  17 //pin dust sensor
-
-#define motor1Pin1 25 //motor
-#define motor1Pin2 26//motor
-
-#define motor2Pin1 32//motor
-#define motor2Pin2 33//motor
-
-#define servo_pin 19
+#define MEASURE_PIN 36 //analog pin
+#define LEDPOWER  19 //pin dust sensor
+#define MOTOR1PIN1 25 //motor
+#define MOTOR1PIN2 26//motor
+#define MOTOR2PIN1 32//motor
+#define MOTOR2PIN2 33//motor
+#define SERVO_PIN 18
+#define TIME_RESET 1
 
 //dust sensor setup
-#define samplingTime 280
-#define deltaTime 40
-#define sleepTime 9680
-
-float voMeasured = 0.0;
-float calcVoltage = 0.0;
-float dustDensity= 0.0;
+#define SAMPLING_TIME 280
+#define DELTA_TIME 40
+#define SLEEP_TIME 9680
 
 //env
 #define WIFI_SSID "" // WiFi Name
@@ -53,37 +47,56 @@ float dustDensity= 0.0;
 #define LINE_TOKEN "" // LINE Token
 
 //debug mode
-#define DEBUG false
+#define DEBUG true
+
+//init jo
+JoSlayer JO;
+
+//dustDensity
+float voMeasured = 0.0;
+float calcVoltage = 0.0;
+float dustDensity= 0.0;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(ledPower,OUTPUT);
-  pinMode(motor1Pin1,OUTPUT);
-  pinMode(motor1Pin2,OUTPUT);
-  pinMode(motor2Pin1,OUTPUT);
-  pinMode(motor2Pin2,OUTPUT);
+  pinMode(LEDPOWER,OUTPUT);
+  pinMode(MOTOR1PIN1,OUTPUT);
+  pinMode(MOTOR1PIN2,OUTPUT);
+  pinMode(MOTOR2PIN1,OUTPUT);
+  pinMode(MOTOR2PIN2,OUTPUT);
+  initWifi();
+  LINE.begin(LINE_TOKEN);
+  JO.Begin(DEBUG,MOTOR1PIN1,MOTOR1PIN2,MOTOR2PIN1,MOTOR2PIN2,SERVO_PIN,TIME_RESET);
+}
+
+void loop(){
+  dustDensity=getDustDensity();
+  if (DEBUG)
+    Serial.printf("dustDensity : %f \n",dustDensity); 
+  if (dustDensity >1000)
+    JO.SmokeDetect();
+  else
+    JO.SmokeReset();
+  delay(1000);
+}
+
+void initWifi(){
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
    while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
-    delay(1000);
+    delay(10);
   }
   Serial.println(WiFi.localIP());
-   LINE.begin(LINE_TOKEN);
-   JO.Begin(DEBUG,motor1Pin1,motor1Pin2,motor2Pin1,motor2Pin2,servo_pin);
 }
 
-void loop(){
-  digitalWrite(ledPower,LOW);
-  delayMicroseconds(samplingTime);
-  voMeasured = analogRead(measurePin); 
-  delayMicroseconds(deltaTime);
-  digitalWrite(ledPower,HIGH);
-  delayMicroseconds(sleepTime);
+float getDustDensity(){
+  digitalWrite(LEDPOWER,LOW);
+  delayMicroseconds(SAMPLING_TIME);
+  voMeasured = analogRead(MEASURE_PIN); 
+  delayMicroseconds(DELTA_TIME);
+  digitalWrite(LEDPOWER,HIGH);
+  delayMicroseconds(SLEEP_TIME);
   calcVoltage = voMeasured * (5.0 / 1024.0);
-  dustDensity = 170 * calcVoltage - 0.1;
-  Serial.println(dustDensity); 
-  if (dustDensity >1000)
-    JO.SomkeDetect();
-  delay(1000);
+  return  170 * calcVoltage - 0.1;
 }
